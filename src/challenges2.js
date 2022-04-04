6372f763855c73a9fe5f70b7b881edf1178ecccf
BrianCaetano <brian-caetano@hotmail.com>
2022-04-04 20:18:33 -0300

    Remove os testes e o README da Trybe

--- src/challenges2.js
+++ src/challenges2.js
@@ -53,6 +53,7 @@ function generatePhoneNumber(arr) {
   primeirosNumeros = primeirosNumeros + "-";
   return `${ddd} ${primeirosNumeros}${ultimosNumeros}`;
 }
+console.log( generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -96,7 +97,7 @@ function hydrate(string) {
 
   return `${soma} ${copo}`;
 }
-
+ console.log(hydrate("1 cerveja, 5 cachaças, 9 vinhos"))
 module.exports = {
   generatePhoneNumber,
   techList,
